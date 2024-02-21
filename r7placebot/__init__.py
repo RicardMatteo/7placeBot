dfbedabc3b03a4ef954c0b03c9e58ea953d57d26
Jo-Blade <bladeur13@free.fr>
2024-02-21 17:35:06 +0100

    configurer l'image et l'offset depuis un fichier de config + fallback automatiquement en headless si pas possible

--- r7placebot/__init__.py
+++ r7placebot/__init__.py
@@ -11,36 +11,56 @@ def read_credentials(file_path):
         password = file.readline().strip()
     return username, password
 
+
+def read_image_config(file_path):
+    with open(file_path, "r") as file:
+        image_path = file.readline().strip()
+        offset_x, offset_y = [int(i) for i in file.readline().strip().split()]
+    return image_path, offset_x, offset_y
+
+
 def main():
-# Chemin du fichier contenant les identifiants
+    # Chemin du fichier contenant les identifiants
     credentials_file = ".env"
 
-# Read the credentials from the file
+    # Chemin du fichier contenant les parametres de l'image
+    config_file = "config.txt"
+
+    # Read the credentials from the file
     username, password = read_credentials(credentials_file)
 
-# Open the website in a new browser window
-    driver = start_chrome('place.inpt.fr', headless=False)
+    # Lire les parametres de l'image depuis le fichier
+    image_path, offset_x, offset_y = read_image_config(config_file)
 
-# Log in to the website
+    # Open the website in a new browser window
+    try:
+        driver = start_chrome('place.inpt.fr', headless=False)
+    except:
+        driver = start_chrome('place.inpt.fr', headless=True)
+
+    # Log in to the website
     write(username, into='Username')
     write(password, into='Password')
     click('Login')
 
-# Wait for the page to load
+    # Wait for the page to load
     time.sleep(5)
 
-# Load the JavaScript code from the file
+    # Load the JavaScript code from the file
     with open("draw.js", "r") as file:
         javascript_code = file.read()
 
-# Get the hex matrix of the image
-    hex_matrix = str(getHexMatrix("mario.png"))
+    # Get the hex matrix of the image
+    hex_matrix = str(getHexMatrix(image_path))
 
-# Replace the placeholder in the JavaScript code with the hex matrix
+    # Replace the placeholder in the JavaScript code with the hex matrix
     javascript_code = javascript_code.replace("MATRIX_TO_DRAW", hex_matrix)
 
+    # Replace the placeholder in the JavaScript code with the correct offsets
+    javascript_code = javascript_code.replace("OFFSET_X", str(offset_x))
+    javascript_code = javascript_code.replace("OFFSET_Y", str(offset_y))
 
-# Execute the JavaScript code
+    # Execute the JavaScript code
     driver.execute_script(javascript_code)
 
     print("Drawing")
