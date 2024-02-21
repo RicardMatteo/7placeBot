d25dbeed0d37a13736c65c40c6f3e428268150d5
Jo-Blade <bladeur13@free.fr>
2024-02-21 11:39:27 +0100

    ça run avec nix-build mais pas encore avec nix run

--- r7placebot/__init__.py
+++ r7placebot/__init__.py
@@ -1,6 +1,6 @@
 from helium import *
 import time
-from imgToMatrix import getHexMatrix
+from r7placebot.imgToMatrix import getHexMatrix
 
 # Helium script to draw the image on the canvas of the website place.inpt.fr
 
@@ -11,35 +11,40 @@ def read_credentials(file_path):
         password = file.readline().strip()
     return username, password
 
+def main():
 # Chemin du fichier contenant les identifiants
-credentials_file = ".env"
+    credentials_file = ".env"
 
 # Read the credentials from the file
-username, password = read_credentials(credentials_file)
+    username, password = read_credentials(credentials_file)
 
 # Open the website in a new browser window
-driver = start_chrome('place.inpt.fr', headless=False)
+    driver = start_chrome('place.inpt.fr', headless=False)
 
 # Log in to the website
-write(username, into='Username')
-write(password, into='Password')
-click('Login')
+    write(username, into='Username')
+    write(password, into='Password')
+    click('Login')
 
 # Wait for the page to load
-time.sleep(5)
+    time.sleep(5)
 
 # Load the JavaScript code from the file
-with open("draw.js", "r") as file:
-    javascript_code = file.read()
+    with open("draw.js", "r") as file:
+        javascript_code = file.read()
 
 # Get the hex matrix of the image
-hex_matrix = str(getHexMatrix("mario.png"))
+    hex_matrix = str(getHexMatrix("mario.png"))
 
 # Replace the placeholder in the JavaScript code with the hex matrix
-javascript_code = javascript_code.replace("MATRIX_TO_DRAW", hex_matrix)
+    javascript_code = javascript_code.replace("MATRIX_TO_DRAW", hex_matrix)
 
 
 # Execute the JavaScript code
-driver.execute_script(javascript_code)
+    driver.execute_script(javascript_code)
 
-print("Drawing")
+    print("Drawing")
+
+
+if __name__ == "__main__":
+    main()
