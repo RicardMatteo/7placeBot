1c6c9ca72f242d2c664651d9c2a7a21f5e0a1fc8
Mattéo Ricard <ricardm@bde.enseeiht.fr>
2024-02-21 11:39:18 +0000

    feat: image support transparency

--- imgToMatrix.py
+++ imgToMatrix.py
@@ -2,14 +2,18 @@ from PIL import Image
 import numpy as np
 
 
-def RGBToHex(r, g, b):
-    return "#{:02x}{:02x}{:02x}".format(r, g, b)
+def RGBToHex(r, g, b, a):
+    return "#{:02x}{:02x}{:02x}".format(r, g, b) if a > 0 else "#gggggg" #gggggg is not a valid color, it is used to represent the transparency
 
 
 def getHexMatrix(image_path):
     with Image.open(image_path, "r") as image:
         pixels = np.array(image)
         hex_matrix = [list(map(lambda x: RGBToHex(x[0], x[1], x[2]), pixels[i])) for i in range(len(pixels))]
+          
+    with open('image', 'w') as file:
+        file.write(str(hex_matrix))
+        
     return hex_matrix
 
 if __name__ == "__main__":
@@ -18,7 +22,7 @@ if __name__ == "__main__":
     with Image.open(image_path, "r") as image:
         #image = image.convert("h")
         pixels = np.array(image)
-        hex_matrix = [list(map(lambda x: RGBToHex(x[0], x[1], x[2]), pixels[i])) for i in range(len(pixels))]
+        hex_matrix = [list(map(lambda x: RGBToHex(x[0], x[1], x[2], x[3]), pixels[i])) for i in range(len(pixels))]
         
     with open('image', 'w') as file:
         file.write(str(hex_matrix))
